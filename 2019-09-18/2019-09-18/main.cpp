ad6d7872b81ec5e2b91af50a289d90c328b9c8ae
Ofashakin <55443255+Ofashakin@users.noreply.github.com>
2019-09-18 08:24:53 -0400

    Add files via upload

---
+++ 2019-09-18/2019-09-18/main.cpp
@@ -0,0 +1,14 @@
+// Name: Oba-ara Fashakin
+// Intro to Computer Science
+// Period 1
+// Mr. Williams
+// Classwork 2019-09-18
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Itadakimas" << std::endl;
+
+
+}
