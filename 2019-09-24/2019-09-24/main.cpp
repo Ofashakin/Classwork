e0002861293597942957df8f3759208865b40952
Ofashakin <55443255+Ofashakin@users.noreply.github.com>
2019-09-24 08:44:13 -0400

    Add files via upload

---
+++ 2019-09-24/2019-09-24/main.cpp
@@ -0,0 +1,20 @@
+// Name: Oba-ara Fashakin
+// Intro to CS
+// Mr. Williams
+// Period 1
+// 2019-09-24-Classwork
+
+#include <iostream>
+
+int main()
+{
+	int Geef = 9;
+
+	std::cout << sizeof(Geef)<< std::endl;
+	char Beef = 3;
+	bool Shmeef = 1;
+	float Leef = 100;
+	double Keef = 2;
+	int Heef = 1000;
+
+}
