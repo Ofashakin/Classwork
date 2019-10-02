d6c1b60620a11e015ede6be2426373467acd39ea
Ofashakin <55443255+Ofashakin@users.noreply.github.com>
2019-10-02 08:43:58 -0400

    Add files via upload

---
+++ 2019-10-02/2019-10-02/main.cpp
@@ -0,0 +1,26 @@
+// Name: Oba-ara Fashakin
+// Intro to CS
+// Mr. Williams
+// Period 1
+// 2019-10-02-Classwork
+#include <iostream>
+
+int main()
+{
+	char Keef = 'K';
+	char Corp = 'C';
+	char Geef = 'G';
+	char Teef = 'T';
+	// These are integers
+	short Heef = 100;
+	int Leef = 1;
+	unsigned long Realboy = 123456789;
+	// These are bools
+	bool isMeat = true;
+	bool isBeef = false;
+
+
+
+
+
+}
