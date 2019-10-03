1bd1f403c60a06302c84807af31ab21775b2ac37
Ofashakin <55443255+Ofashakin@users.noreply.github.com>
2019-10-03 08:42:46 -0400

    Add files via upload

---
+++ 2019-10-03-Classwork/2019-10-03-Classwork/main.cpp
@@ -0,0 +1,18 @@
+// Name: Oba-ara Fashakin
+// Intro to CS
+// Period 1
+// Mr. Williams
+// 2019-10-03-Classwork
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Enter your favorite number: ";
+	int favoriteNumber;
+	std::cin >> favoriteNumber;
+	std::cout << "Cool, " << favoriteNumber << " is my favorite number too!" << std::endl;
+
+
+}
