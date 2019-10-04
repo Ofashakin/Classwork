f18de6a517aca75fd0c5475bde9ae9babead8483
Ofashakin <55443255+Ofashakin@users.noreply.github.com>
2019-10-04 08:47:23 -0400

    Add files via upload

---
+++ 2019-10-04-Classwork/2019-10-04-Classwork/main.cpp
@@ -0,0 +1,38 @@
+// Name: Oba-ara Fashakin
+// Intro to CS
+// Period 1
+// Mr. Williams
+// 2019-10-04-Classwork
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// 3
+	float pi = 4 * (1.0 - (1 / 3.0) + (1 / 5.0) - (1 / 7.0) + (1 / 9.0) - (1 / 11.0));
+	float pi2 = 4 * (1.0 - (1 / 3.0) + (1 / 5.0) - (1 / 7.0) + (1 / 9.0) - (1 / 11.0) + (1 / 13.0));
+	cout << "Pi = " << pi << endl;
+	cout << "Pi2 = " << pi2 << endl;
+
+	// 4
+	float c;
+	cout << "Enter a degree in Celsius, young scholar: ";
+	cin >> c;
+	float f = (9 / 5.0) * c + 32;
+	cout << c << "in Fahrenheit is " << f << endl;
+
+	// 1
+	float avspd;
+
+	// Homework P1
+	float rad = 5.5;
+	float circum = 2 * rad * pi;
+	float area = 2 * rad * rad * pi;
+	cout << "Circumference = " << circum << endl;
+	cout << "Area = " << area << endl;
+
+	// Homework P2
+	float perim = (2 * 4.5) + (2 * 7.9)
+	
+}
