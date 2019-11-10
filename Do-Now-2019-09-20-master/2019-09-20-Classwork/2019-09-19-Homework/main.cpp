14762d9880d8ec0254a2fb3323658468525ae90f
Ofashakin <55443255+Ofashakin@users.noreply.github.com>
2019-11-09 20:44:15 -0500

    Update main.cpp

--- Do-Now-2019-09-20-master/2019-09-20-Classwork/2019-09-19-Homework/main.cpp
+++ Do-Now-2019-09-20-master/2019-09-20-Classwork/2019-09-19-Homework/main.cpp
@@ -12,12 +12,12 @@
 int main()
 {std::cout	<< "Name: Mr. Williams" << std::endl << "Class Schedule" << std::endl
 				<< "Period 1: Intro to CS" <<std::endl
-				<< "Period 2: Earth Science" << std::endl
-				<< "Period 3: PE" << std::endl
-				<< "Period 4: Global History" << std::endl
-				<< "Period 5: Visual Arts" << std::endl
-				<< "Period 6: Lunch" << std::endl
-				<< "Period 7: Geometry" << std::endl
-				<< "Period 8: House" << std::endl
-				<< "Period 9: Health" << std::endl
-				<< "Period 10: Spanish";}
+				<< "Period 2: Phys.Ed." << std::endl
+				<< "Period 3: Health" << std::endl
+				<< "Period 4: House" << std::endl
+				<< "Period 5: Algebra II" << std::endl
+				<< "Period 6: English 10 Hnrs" << std::endl
+				<< "Period 7: Lunch" << std::endl
+				<< "Period 8: Global History I" << std::endl
+				<< "Period 9: Spanish" << std::endl
+				<< "Period 10: Earth Science";}
